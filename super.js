04e6f8319d3622a809f165fd96a9803464e1456a
Bailey <priest2105@gmail.com>
2024-09-19 23:42:04 +0100

    commit ✨✨

--- super.js
+++ super.js
@@ -29,34 +29,6 @@ const symbolValues = {
 
 
 
-const spin = () => {
-
-    const symbols = [ ]
-
-    for(const [symbol, count] of Object.entries(symbolsCount)){
-        for(let i = 0; i < count; i++){
-            symbols.push(symbol);
-         }
-     }
-     
-
-     const reels = [[], [], []]
-
-     for( let i = 0; i < COLS; i++){
-        const reelSymbols = [...symbols];
-        for( let j = 0; j < ROWS; j++){
-
-        }
-     }
-
-    }
-
-
-
-
-spin();
-
-
 
 
 const deposit = () => {
@@ -107,6 +79,39 @@ const getBet = (balance, lines) => {
 
 };
 
+
+
+const spin = () => {
+
+    const symbols = [ ]
+
+    for(const [symbol, count] of Object.entries(symbolsCount)){
+        for(let i = 0; i < count; i++){
+            symbols.push(symbol);
+         }
+     }
+     
+
+     const reels = [[], [], []]
+
+     for( let i = 0; i < COLS; i++){
+        const reelSymbols = [...symbols];
+        for( let j = 0; j < ROWS; j++){
+             const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+            const selectedSymbol = reelSymbols[randomIndex]; 
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(randomIndex, 1);
+        }
+     }
+
+     return reels;
+    }
+
+
+
+
+const reels = spin();
+console.log(reels);
 
 let  balance = deposit(); 
 const numberOfLines =  getNumberofLines();
