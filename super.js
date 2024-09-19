e4bc2253348e60facf5993a67f5e507efef4edf0
Bailey <priest2105@gmail.com>
2024-09-19 22:52:50 +0100

    commit

---
+++ super.js
@@ -0,0 +1,71 @@
+// 1.  Deposit some money
+// 2. Detemrnie number of lines to bet on
+// 3. Collect a bet Amount
+// 4. Spin the slot Machine
+// 5. Check if user won
+// 6. Give user thier winnings
+// 7. Play again.
+
+const prompt = require("prompt-sync")();
+
+
+
+
+
+
+
+
+
+
+const deposit = () => {
+
+    while (true) {
+        
+    const depositAmount = prompt(" Enter a Deposit Amount: ");
+
+    const numberDepositAmount = parseFloat(depositAmount);
+
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0 ){
+        console.log("invalid Deposit Amount Try again.");
+    }else{
+        return numberDepositAmount;
+    }   
+ }
+};
+
+const getNumberofLines = () => {
+
+    while (true) {
+    const lines = prompt("Ener the Number of Lines you want to bet on, within range (1 - 3): ")
+    const numberOfLines = parseFloat(lines);
+
+    if ( isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3){
+        console.log("Invalid Number of Lines Try again.");
+    }else{
+        return numberOfLines;
+    }
+  }
+};
+
+const getBet = (balance, lines) => {
+
+    while (true) {
+        
+        const bet = prompt("Enter the bet per line : ")
+        const numberBet = parseFloat(bet);
+
+        if ( isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+            console.log("Invalide bet amount. Try again");
+        }
+        else{
+            return numberBet;
+        }
+    }
+
+
+};
+
+
+let  balance = deposit(); 
+const numberOfLines =  getNumberofLines();
+const bet = getBet(balance, numberOfLines);
